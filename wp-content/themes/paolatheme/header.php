063dfd81687326cd21a875d1325a1312670fb507
paola-carta <paola.carta1982@gmail.com>
2017-05-24 17:14:00 +0200

    Add files via upload

---
+++ wp-content/themes/paolatheme/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<?php wp_head(); ?>
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  <div class="jumbotron">
+    <div class="container">
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"></a>
+          </div>
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">Cookies</a></li>
+              <li><a href="#">Pastries</a></li>
+              <li><a href="#">Cakes</a></li>
+              <li><a href="#Contact">Contact us</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 welcome">
+            <h1>For a sweeter day</h1>
+            <h2>book now a taste of sweetness</h2>
+          </div>
+        </div>
+      </nav>
+
--- wp-content/themes/paolatheme/index.php
+++ wp-content/themes/paolatheme/index.php
@@ -1,43 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title></title>
-  <link rel="stylesheet" type="text/css" href="vendors/css/bootstrap.min.css">
- <link rel="stylesheet" type="text/css" href="css/main.css">
- <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-  <div class="jumbotron">
-    <div class="container">
-      <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#"></a>
-          </div>
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right">
-              <li><a href="#">Cookies</a></li>
-              <li><a href="#">Pastries</a></li>
-              <li><a href="#">Cakes</a></li>
-              <li><a href="#Contact">Contact us</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12 welcome">
-            <h1>For a sweeter day</h1>
-            <h2>book now a taste of sweetness</h2>
-          </div>
-        </div>
-      </nav>
-    </div>
-  </div>
+<?php get_header(); ?>
   <div class="container-fluid" id="Contact">
     <div class="row text-center">
       <h1>Contact us</h1>
@@ -64,10 +25,5 @@
         </div>
       </form>
     </div>
-    <script type="text/javascript" src="vendors/js/jquery.min.js"></script>
-    <script type="text/javascript" src="vendors/js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
   </div>
-</body>
-</html>
-
+<?php get_footer(); ?>
