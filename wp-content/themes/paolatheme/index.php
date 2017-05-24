e3239ca66122e430b5f0fbd3c1c2af4f8b5f58ce
paola-carta <paola.carta1982@gmail.com>
2017-05-24 16:38:00 +0200

    Add files via upload

--- wp-content/themes/paolatheme/index.php
+++ wp-content/themes/paolatheme/index.php
@@ -1,1 +1,73 @@
-<?php  ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="vendors/css/bootstrap.min.css">
+ <link rel="stylesheet" type="text/css" href="css/main.css">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  <div class="jumbotron">
+    <div class="container">
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"></a>
+          </div>
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">Cookies</a></li>
+              <li><a href="#">Pastries</a></li>
+              <li><a href="#">Cakes</a></li>
+              <li><a href="#Contact">Contact us</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 welcome">
+            <h1>For a sweeter day</h1>
+            <h2>book now a taste of sweetness</h2>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </div>
+  <div class="container-fluid" id="Contact">
+    <div class="row text-center">
+      <h1>Contact us</h1>
+      <h2>to receive a sweet gift</h2>
+    </div>
+    <div class="row">
+      <form class="paola-form">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Please, type here your email address" required autofocus>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Please, insert your password" required>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <label for="exampleInputPassword1">Message</label>
+          <textarea class="form-control" placeholder="Please, write a message" rows="3"></textarea>
+        </div>
+        <div class="col-md-12 text-center">
+          <button type="submit" class="btn btn-default">Submit</button>
+        </div>
+      </form>
+    </div>
+    <script type="text/javascript" src="vendors/js/jquery.min.js"></script>
+    <script type="text/javascript" src="vendors/js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+  </div>
+</body>
+</html>
+
