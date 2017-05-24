731815ababf6c449be19bec3a78c78730debdefe
paola-carta <paola.carta1982@gmail.com>
2017-05-24 20:04:55 +0200

    Add files via upload

--- wp-content/themes/paolatheme/header.php
+++ wp-content/themes/paolatheme/header.php
@@ -1,34 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
-	<?php wp_head(); ?>
- <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
 </head>
 <body>
-  <div class="jumbotron">
-    <div class="container">
-      <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#"></a>
+    <div class="jumbotron">
+      <div class="container">
+        <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#"></a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Cookies</a></li>
+                <li><a href="#">Pastries</a></li>
+                <li><a href="#">Cakes</a></li>
+                <li><a href="#Contact">Contact us</a></li>
+              </ul>
+            </div>
           </div>
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right">
-              <li><a href="#">Cookies</a></li>
-              <li><a href="#">Pastries</a></li>
-              <li><a href="#">Cakes</a></li>
-              <li><a href="#Contact">Contact us</a></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
-   </div>
-
+        </nav> 
--- wp-content/themes/paolatheme/index.php
+++ wp-content/themes/paolatheme/index.php
@@ -1,35 +1,39 @@
 <?php get_header(); ?>
-        <div class="row">
-          <div class="col-md-12 welcome">
-            <h1><?php bloginfo('name') ?></h1>
-            <h2><?php bloginfo('description') ?></h2>
-          </div>
-        </div>
-  <div class="container-fluid" id="Contact">
-    <div class="row text-center">
+<div class="row">
+    <div class="col-md-12 welcome">
+        <h1><?php bloginfo('name') ?></h1>
+        <h2><?php bloginfo('description') ?></h2>
+    </div>
+</div>
+</div>
+</div>
+<div class="row" id="contact">
+    <div class="col-lg-12 text-center">
       <h1>Contact us</h1>
       <h2>to receive a sweet gift</h2>
     </div>
-    <div class="row">
-      <form class="paola-form">
-        <div class="col-md-6">
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Please, type here your email address" required autofocus>
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Please, insert your password" required>
-          </div>
-        </div>
-        <div class="col-md-6">
-          <label for="exampleInputPassword1">Message</label>
-          <textarea class="form-control" placeholder="Please, write a message" rows="3"></textarea>
-        </div>
-        <div class="col-md-12 text-center">
-          <button type="submit" class="btn btn-default">Submit</button>
-        </div>
-      </form>
-    </div>
   </div>
+  <div class="row">
+    <form class="paola-form">
+      <div class="col-md-6">
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Please, type here your email address" required autofocus>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Please, insert your password" required>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <label for="exampleInputPassword1">Message</label>
+        <textarea class="form-control" placeholder="Please, write a message" rows="3"></textarea>
+      </div>
+      <div class="col-md-12 text-center">
+        <button type="submit" class="btn btn-default">Submit</button>
+      </div>
+    </form>
+  </div>
+</div>
 <?php get_footer(); ?>
+
